7252264d0da5c7e20eb240d2bc2c94cfb3b06288
gandaraipower <jjangjongsoo@naver.com>
2025-09-12 12:45:44 +0900

    13강

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,28 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
 export default function Home() {
-  return <div>여기는 /posts 페이지입니다.</div>;
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-9">
+      <h1>글 목록</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>{post.id} : {post.subject}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
