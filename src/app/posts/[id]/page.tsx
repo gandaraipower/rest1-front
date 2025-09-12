96c06bbbe4292112a8cfc57e24192a7d685fa13f
gandaraipower <jjangjongsoo@naver.com>
2025-09-12 15:15:01 +0900

    17강

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
+import {PostDto} from "@/type/post";
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';   
 
 export default function Home() {
-    const {id}=useParams(2);
+    const {id}=useParams();
 
-    const [post, setPost] = useState<{ 
-        id: number;
-        subject: string;
-        body: string;
-     } | null>(null);
+    const [post, setPost] = useState<PostDto | null>(null);
   
     async function fetchPost() {
       const res = await fetch(`http://localhost:8080/api/v1/posts/${id}`);
---
+++ src/type/post.ts
@@ -0,0 +1,7 @@
+export type PostDto = {
+    id: number;
+    subject: string;
+    body: string;
+    createDate: string;
+    modifyDate: string;
+}
